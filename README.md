073e99a9a886e33f9ba820021332d39e226c8f77
Joni Hasanen <joni@pieceofcode.com>
2024-09-05 15:35:34 +0300

    Led blinking

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Following [The Rusty Bits](https://www.youtube.com/@therustybits)-channels video
 Going to link videos here, which I've done so far, just to keep track
 
 - Setup: https://www.youtube.com/watch?v=TOAynddiu5M
-  - I'm using microbit version 1.5, so needed to check some configs from here https://github.com/nrf-rs/microbit/blob/main/.cargo/config.toml#L6
+  - I'm using micro:bit version 1.5, so needed to check some configs from here https://github.com/nrf-rs/microbit/blob/main/.cargo/config.toml#L6
   - Also needed to add `defmt` and `defmt-rtt` crates
   - macos gdb: `brew install arm-non-eabi-gdb`
+- Blinking a LED: https://www.youtube.com/watch?v=A9wvA_S6m7Y
+  -Some adjustemnts needed for micro:bit v1.5
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 #![no_std]
 #![no_main]
 
+use core::ptr::write_volatile;
+
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let mut x: usize = 0;
+    /* COL1 pin is 04, ROW1 pin is 13, source: https://github.com/microbit-foundation/microbit-reference-design/blob/master/PDF/Schematic%20Print/Schematic%20Prints.PDF
+        Addresses can found in https://infocenter.nordicsemi.com/pdf/nRF51_RM_v3.0.pdf page 57
+    */
+    const GPIO0_PINCNF13_ROW1_ADDR: *mut u32 = 0x5000_0734 as *mut u32;
+    const GPIO0_PINCNF04_COL1_ADDR: *mut u32 = 0x5000_0710 as *mut u32;
+    const DIR_OUTPUT_POS: u32 = 0;
+    const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POS;
+    unsafe {
+        write_volatile(GPIO0_PINCNF13_ROW1_ADDR, PINCNF_DRIVE_LED);
+        write_volatile(GPIO0_PINCNF04_COL1_ADDR, PINCNF_DRIVE_LED);
+    }
+    const GPIO0_OUT_ADDR: *mut u32 = 0x5000_0504 as *mut u32;
+    const GPIO0_OUT_ROW1_POS: u32 = 13;
+    let mut is_on = false;
     loop {
-        x += 1;
-        for _ in 0..x {
+        unsafe {
+            write_volatile(GPIO0_OUT_ADDR, (is_on as u32) << GPIO0_OUT_ROW1_POS);
+        }
+        for _ in 0..400_000 {
             nop();
         }
+        is_on = !is_on;
     }
 }
